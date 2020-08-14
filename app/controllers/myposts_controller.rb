753bae45aefe2c821f3106030f7d17a43196de95
noriko ogihara <aloha.nori1104@gmail.com>
2020-08-14 17:35:01 +0900

    バリデーション機能を追加

--- app/controllers/myposts_controller.rb
+++ app/controllers/myposts_controller.rb
@@ -9,6 +9,21 @@ class MypostsController < ApplicationController
   end
 
   def create
+    @mypost = Mypost.create(mypost_params)
+    if @mypost.save
+      redirect_to new_mypost_path, notice: "投稿しました！"
+    else
+      render :new
+    end
+  end
 
+  def show
+    params[:id]
+  end
+
+  private
+
+  def mypost_params
+    params.require(:mypost).permit(:content, :created_at)
   end
 end
--- app/models/mypost.rb
+++ app/models/mypost.rb
@@ -1,2 +1,3 @@
 class Mypost < ApplicationRecord
+  validates :content, length: {in: 1..140}
 end
