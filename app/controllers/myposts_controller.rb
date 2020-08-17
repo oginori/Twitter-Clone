c1496263e75cb3fd97afd73c11e544601ac9a573
noriko ogihara <aloha.nori1104@gmail.com>
2020-08-17 12:56:16 +0900

    パーシャルを作成

--- app/controllers/myposts_controller.rb
+++ app/controllers/myposts_controller.rb
@@ -1,5 +1,5 @@
 class MypostsController < ApplicationController
-
+  before_action :set_mypost, only: [:edit, :update, :destroy]
   def index
     @mypost = Mypost.all
   end
@@ -10,29 +10,45 @@ class MypostsController < ApplicationController
 
   def create
     @mypost = Mypost.create(mypost_params)
-    if @mypost.save
-      redirect_to new_mypost_path, notice: "投稿しました！"
-    else
+    if params[:back]
       render :new
+    else
+      if @mypost.save
+        redirect_to myposts_path, notice: "投稿しました！"
+      else
+        render :new
+      end
     end
   end
 
   def edit
-    @mypost = Mypost.find(params[:id])
   end
 
-  def uptdate
-    @mypost = Mypost.find(params[:id])
+  def update
     if @mypost.update(mypost_params)
-      redirect_to mypost_path, notice: "編集しました！"
+      redirect_to myposts_path, notice: "編集しました！"
     else
       render :edit
     end
+  end
+
+  def destroy
+    @mypost.destroy
+    redirect_to myposts_path, notice: "削除しました！"
+  end
+
+  def confirm
+    @mypost = Mypost.new(mypost_params)
+    render :new if @mypost.invalid?
   end
 
   private
 
   def mypost_params
     params.require(:mypost).permit(:content, :created_at)
+  end
+
+  def set_mypost
+    @mypost = Mypost.find(params[:id])
   end
 end
--- app/helpers/myposts_helper.rb
+++ app/helpers/myposts_helper.rb
@@ -1,2 +1,9 @@
 module MypostsHelper
+  def choose_new_or_edit
+    if action_name == 'new' || action_name == 'create'
+      confirm_myposts_path
+    elsif action_name == 'edit'
+      mypost_path
+    end
+  end
 end
